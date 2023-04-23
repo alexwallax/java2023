cddfe482c5a8d7d7312203bdccd9f79133148ec2
alexwallax <alexwallax@gmail.com>
2023-04-23 11:23:54 -0300

    Wrapper

---
+++ src/fundamentos/Wrapper.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		
+		// Wrapper versão objeto dos tipos primitivos
+		Byte b = 100;
+		Short s = 1000;
+		//Integer i = 10000;
+		//Long l = 100000L;
+		
+		System.out.println(b.byteValue());
+		System.out.println(s.toString()); // tranforma o valor em String
+		
+		
+		
+
+	}
+
+}
