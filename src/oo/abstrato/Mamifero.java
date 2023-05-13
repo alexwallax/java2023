59628217e6d61bad1f57de03f1a7d5300447af35
Alex Wallace <alexwallax@gmail.com>
2023-05-13 10:55:20 -0300

    Commit abstract

---
+++ src/oo/abstrato/Mamifero.java
@@ -0,0 +1,14 @@
+package oo.abstrato;
+
+public abstract class Mamifero extends Animal{
+	
+	public abstract String mamar();
+	
+	@Override
+	public String mover() {
+		// TODO Auto-generated method stub
+		return "Saindo do lugar";
+	}
+	
+	
+}
